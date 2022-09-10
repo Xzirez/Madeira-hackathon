f14a972280f1a66dd1f36c05849147b002c14c6b
Xzirez <33001507+Xzirez@users.noreply.github.com>
2022-09-10 18:25:00 +0100

    feat: personal account page (#8)
    Co-authored-by: VetleFell <vetle.stubberud@gmail.com>

--- src/pages/PersonalAccount.js
+++ src/pages/PersonalAccount.js
@@ -1,5 +1,75 @@
+import { Center, Box, Heading, Avatar } from '@chakra-ui/react';
 const PersonalAccount = () => {
-  return <></>;
+  return (
+    <Center>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        paddingTop="80px"
+        flexDirection="column"
+        gap="35px"
+      >
+        <Heading size="2xl">John Johnson</Heading>
+        <Avatar size="2xl"></Avatar>
+        <Box
+          gap="25px"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Heading
+            cursor="pointer"
+            size="lg"
+            width="290px"
+            borderBottom="1px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            paddingBottom="20px"
+          >
+            Passport
+          </Heading>
+          <Heading
+            cursor="pointer"
+            size="lg"
+            width="290px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            borderBottom="1px"
+            paddingBottom="10px"
+          >
+            Drivers License
+          </Heading>
+          <Heading
+            cursor="pointer"
+            size="lg"
+            width="290px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            borderBottom="1px"
+            paddingBottom="10px"
+          >
+            COVID-19
+          </Heading>
+          <Heading
+            cursor="pointer"
+            size="lg"
+            width="290px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            borderBottom="1px"
+            paddingBottom="10px"
+          >
+            Car Insurance
+          </Heading>
+        </Box>
+      </Box>
+    </Center>
+  );
 };
 
 export default PersonalAccount;
